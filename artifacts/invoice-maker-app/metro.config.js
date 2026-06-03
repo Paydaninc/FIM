cac20ce79a057b24e051a8bb934c3b4288040ad3
Paydaninc <info@paydan.net>
2026-06-03 15:07:33 -0700

    sync: artifacts/invoice-maker-app/metro.config.js

---
+++ artifacts/invoice-maker-app/metro.config.js
@@ -0,0 +1,13 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  "@workspace/api-client-react": path.resolve(
+    __dirname,
+    "lib/api-client-react/src"
+  ),
+};
+
+module.exports = config;
